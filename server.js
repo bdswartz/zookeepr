77c72815a3dc1ff154a6e0dcfb49041e765d57ee
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-03-05 07:30:20 -0500

    connected the get and post links from front end pages to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));  
 const fs = require('fs');
 const path = require('path');
 const { animals } = require('./data/animals');
@@ -106,6 +107,20 @@ app.post('/api/animals', (req, res) => {
       res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
